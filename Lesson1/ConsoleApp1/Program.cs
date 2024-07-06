9829e4fc25e59e65dfbd271a5128f8e87d4f1600
muxtarovArif <departed2015@gmail.com>
2024-07-06 14:55:36 +0400

    C# project updated

--- Lesson1/ConsoleApp1/Program.cs
+++ Lesson1/ConsoleApp1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        String userInput = Console.ReadLine();
+        Console.WriteLine(userInput);
+    }
+}
